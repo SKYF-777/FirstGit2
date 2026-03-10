ea147020c689bf5ec9108cc6ca060bb6d8809833
SKYF-777 <hipster25565@gmail.com>
2026-03-11 00:53:11 +0300

    добавил метод удаления содержимого файла

--- src/corparation/OperationCode.kt
+++ src/corparation/OperationCode.kt
@@ -1,5 +1,8 @@
 package corparation
 
 enum class OperationCode (val title: String) {
-    EXIT("Exit"), REGISTER_NEW_ITEM("Register new item"), SHOW_ALL_ITEMS("Show all items")
+    EXIT("Exit"),
+    REGISTER_NEW_ITEM("Register new item"),
+    SHOW_ALL_ITEMS("Show all items"),
+    REMOVE_PRODUCT_CARD("remove product card")
 }
--- src/tasks/Car.kt
+++ src/tasks/Car.kt
@@ -1,35 +1,9 @@
 package tasks
 
 class Car(
-    val brand: String,
+    val make: String,
     val model: String,
-    val enginePower: Int,
-    val bodyColor: String
-) {
-
-    fun printInfo(){
-        println("$brand $model $enginePower л.с. $bodyColor")
-    }
-
-    fun drive() {
-        if (enginePower >= 120) {
-            println("Еду быстро, но недалеко на $brand $model")
-        } else {println("Еду далеко, но небыстро на $brand $model")}
-    }
-
-    fun refuel(Name: String, markBenz: String, count: Int) {
-        println("Произведена заправка на АЗС \"$Name\"")
-        println("Марка бензина: $markBenz")
-        println("Кол-во литров: $count")
-    }
-}
-
-fun task() {
-    val input = readln().split(" ")
-    val inputBrand = input[0]
-    val inputModel = input[1]
-    val inputEngPower = input[2].toInt()
-    val inputColor = input[3]
-    val car = Car(brand = inputBrand, model = inputModel, enginePower = inputEngPower, bodyColor = inputColor)
-    car.drive()
-}
+    val year: Int,
+    val vin: String,
+    val color: String,
+)
